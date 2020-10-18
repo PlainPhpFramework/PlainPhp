feaa9b8ef1a281d73f473066deef73e344c47607
Francesco Terenzani <f.terenzani@gmail.com>
2020-10-18 23:47:34 +0200

    Tab 2 space indent

--- src/Mail.php
+++ src/Mail.php
@@ -2,21 +2,21 @@
 /**
  * (c) 2020 Francesco Terenzani
  */
-namespace PlainPhp;
+namespace pp;
 
 class Mail
 {
 
-	static function send($template, $data)
-	{
-		$mailer = get('config/mailer.php');
-		$render = function($template, $data) {
-			extract($data);
-			require $template;
-		};
-		$message = Closure::bind($render, $mailer);
-		$message($template, $data);
-		return $mailer->send();
-	}
+    static function send($template, $data)
+    {
+        $mailer = get('config/mailer.php');
+        $render = function($template, $data) {
+            extract($data);
+            require $template;
+        };
+        $message = Closure::bind($render, $mailer);
+        $message($template, $data);
+        return $mailer->send();
+    }
 
 }
